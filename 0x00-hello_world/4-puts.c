d7026ad4ad9a631c520908808c711477777e3d52
Kamerelinda <kamerelinda14@gmail.com>
2023-03-16 23:21:01 +0000

    function puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+/**
+ * main -Entry point
+ * Description-use the function puts()
+ * program ends with 0
+ * Return:Always 0 (Success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
