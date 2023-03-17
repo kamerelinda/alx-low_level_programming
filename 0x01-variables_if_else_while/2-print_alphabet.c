8be7fa31bb5a6bbe75482ee616a59a2f92498f4d
Kamerelinda <kamerelinda14@gmail.com>
2023-03-17 21:42:26 +0000

    prints alphabet in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main-Entry point
+ * prints alphabet in new line
+ * Return: Always 0 (Success)
+ * **/
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+
+	return (0);
+}
