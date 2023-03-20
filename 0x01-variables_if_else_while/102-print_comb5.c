c1eba6c909df4d1f3adaf24dfe62a14b2c2a486c
Kamerelinda <kamerelinda14@gmail.com>
2023-03-20 20:36:31 +0000

    prints combination of two two-digit without repeat

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,9 +11,9 @@ int main(void)
 	int i;
 	int j;
 
-	for (i = 0; i <=99; i++)
+	for (i = 0; i <= 99; i++)
 	{
-		for (j=0; j <=99; j++)
+		for (j = 0; j <= 99; j++)
 		{
 			if (i < j && i != j)
 			{
@@ -22,7 +22,7 @@ int main(void)
 				putchar(' ');
 				putchar((j / 10) + '0');
 				putchar((j % 10) + '0');
-				if (i != 98 || j !=99)
+				if (i != 98 || j != 99)
 				{
 					putchar(',');
 					putchar(' ');
