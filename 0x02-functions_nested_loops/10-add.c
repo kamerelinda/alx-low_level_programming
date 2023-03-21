1f184d008693a9d3469cd75e590341090ac85ef0
Kamerelinda <kamerelinda14@gmail.com>
2023-03-21 20:09:28 +0000

    adds two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ *add -prints sum of two numbers
+ *@a: one of the integers
+ *@b: other integer
+ *Return: Always 0 (success)
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
