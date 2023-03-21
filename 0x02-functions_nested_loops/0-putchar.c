a9984d8686d2109f595b1f205bb68d7145971e42
Kamerelinda <kamerelinda14@gmail.com>
2023-03-21 12:42:32 +0000

    0._putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main-Entry point
+ * prints _putchar followed by a new line
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char *c = "_putchar";
+
+	while (*c)
+	}
+		_putchar(*c);
+		c++;
+	}
+	_putchar('\n');
+	return (0);
+}
