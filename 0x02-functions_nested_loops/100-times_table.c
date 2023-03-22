a56c687ff1a0d523dabcadc59b9fb1cd2c661811
Kamerelinda <kamerelinda14@gmail.com>
2023-03-22 20:30:25 +0000

    spacing

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -18,15 +18,21 @@ void print_times_table(int n)
 				r = i * j;
 				if (r < 10)
 				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
 					_putchar(r + '0');
 				}
-				if (r < 100)
+				else if (r < 100)
 				{
+					_putchar(' ');
+					_putchar(' ');
 					_putchar(r / 10 + '0');
 					_putchar(r % 10 + '0');
 				}
-				else
+				else if (r >= 100)
 				{
+					_putchar(' ');
 					_putchar(r / 100 + '0');
 					_putchar((r / 10) % 10 + '0');
 					_putchar(r % 10 + '0');
@@ -34,10 +40,8 @@ void print_times_table(int n)
 				if (j < n)
 				{
 				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
 				}
-			}	
+			}
 			_putchar('\n');
 		}
 	}
