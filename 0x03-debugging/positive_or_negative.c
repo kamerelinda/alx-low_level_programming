5a737db27c1845b9b57097a55b8dde362f4c0505
Kamerelinda <kamerelinda14@gmail.com>
2023-03-23 08:45:32 +0000

    multiple mains

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -3,23 +3,23 @@
 #include <stdio.h>
 
 /**
- * main-Entry point
- * apply if conditional statement to say if n is negative or positive
- * Return: Always 0 (success)
+ * positive_or_negative-statement to say if n is negative or positive
+ * @n:inputed number
+ * Return: void (success)
  * **/
 void positive_or_negative(int n)
 {
-        if (n > 0)
-        {
-                printf("%d is positive\n", n);
-        }
-        if (n == 0)
-        {
-                printf("%d is zero\n", n);
-        }
-        if (n < 0)
-        {
-                printf("%d is negative\n", n);
-        }
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	if (n < 0)
+	{
+		printf("%d is negative\n", n);
+	}
 }
 
