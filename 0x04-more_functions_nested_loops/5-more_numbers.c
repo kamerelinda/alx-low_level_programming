594bcfed4b67f9a03a73cb590375601c60decd83
Kamerelinda <kamerelinda14@gmail.com>
2023-03-23 20:05:32 +0000

    prints ten times

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,24 +6,19 @@
  */
 void more_numbers(void)
 {
-	int i = 0;
+	int i;
 	int j;
 
-	while (i <= 9)
+	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j < 15; j++)
+		for (j = 0; j <= 14; j++)
 		{
-			if (j < 10)
+			if (j > 9)
 			{
-				_putchar(j + '0');
+				_putchar((j / 10) + '0');
 			}
-			else if (j >= 10 && j < 100)
-			{
-				_putchar(j / 10 + '0');
-				_putchar(j % 10 + '0');
-			}
+				_putchar((j % 10) + '0');
 		}
-		i++;
 		_putchar('\n');
 	}
 }
