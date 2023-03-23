0c4e6aa96a86fd3cff110d5d934614c0f1d6f109
Kamerelinda <kamerelinda14@gmail.com>
2023-03-23 22:23:53 +0000

    Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -7,13 +7,12 @@
 int main(void)
 {
 	int i;
-	char* a= "Fizz";
-	char* b= "Buzz";
-	char* c= "FizzBuzz";
+	char *a = "Fizz";
+	char *b = "Buzz";
+	char *c = "FizzBuzz";
 
-	for (i = 1; i <=100; i++)
+	for (i = 1; i <= 100; i++)
 	{
-	
 		if ((i % 3 == 0) && (i % 5 == 0))
 		{
 			printf("%s ", c);
