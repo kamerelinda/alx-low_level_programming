4ea27364d9097f8f637f74b108b5527793fb4b8e
Kamerelinda <kamerelinda14@gmail.com>
2023-03-24 08:39:12 +0000

    prime numbers

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *Return: Always 0 success
+ */
+int main(void)
+{
+	long n = 612852475143;
+	long i;
+	for (i = 2; i * i <= n; i++)
+	{
+		while (n % i == 0) 
+		{
+			n /= i;
+		}
+	}
+
+	if (n > 1)
+	{
+		printf("%ld\n", n);
+	}
+	return 0;
+}
