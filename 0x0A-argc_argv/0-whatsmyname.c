eac2316166be08fcf59d28941cb9c5b827493f3b
Kamerelinda <kamerelinda14@gmail.com>
2023-04-10 20:12:06 +0000

    prints only name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,17 +6,13 @@
  *
  *@argc: count of the arguments entered
  *
- *@argv: array of strings
+ *@argv
+ ne!
  *
  * Return: Always 0 (success)
  */
-int main(int argc, char *argv[])
+int main(int __attribute__ ((unused)) argc, char *argv[])
 {
-	int i;
-
-	for (i = 0; i < argc; i++)
-	{
-		printf("%s\n", argv[i]);
-	}
+	printf("%s\n", argv[0]);
 	return (0);
 }
