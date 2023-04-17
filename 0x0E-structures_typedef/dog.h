4b38fec080c979cf755a886d0b7d6efd3d5b9044
Kamerelinda <kamerelinda14@gmail.com>
2023-04-17 21:46:12 +0000

    define a new name

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,14 @@
 #ifndef DOG_H
 #define DOG_H
+/**
+ * struct dog - struct for a dog
+ *
+ * @name: name of dog
+ *
+ * @age: age of dog
+ *
+ * @owner: owner of dog
+ */
 struct dog
 {
 	char *name;
