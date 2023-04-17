e9dbe5f3fc54e36c0533e02ca95d329c86b403bf
Kamerelinda <kamerelinda14@gmail.com>
2023-04-17 20:37:43 +0000

    define a new type struct dog

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,2 @@
+A structure is a user defined data type that groups variables of differnt data types under one name.
+Typedef is used to an alias of another stat type
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * struct dog - new structure with dog tag
+ *
+ * @name: character pointer
+ * @age: float number
+ * @owner: character pointer
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
