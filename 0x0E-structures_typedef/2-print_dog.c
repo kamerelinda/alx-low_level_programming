a8e688508658c368972385efad16222a369ca7d0
Kamerelinda <kamerelinda14@gmail.com>
2023-04-17 21:56:23 +0000

    prints a struct

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -15,20 +15,12 @@ void print_dog(struct dog *d)
 	if (d != NULL)
 	{
 		printf("Name: ");
-		if (d->name == NULL)
-		{
-			printf("(nil)\n");
-		}
-		printf("%s\n", d->name);
+		d->name == NULL ? printf("(nil)\n") : printf("%s\n", d->name);
 
 		printf("Age: ");
 		printf("%f\n", d->age);
 
 		printf("Owner: ");
-		if (d->owner == NULL)
-		{
-			printf("(nil)\n");
-		}
-		printf("%s\n", d->owner);
+		d->owner == NULL ? printf("(nil)\n") : printf("%s\n", d->owner);
 	}
 }
