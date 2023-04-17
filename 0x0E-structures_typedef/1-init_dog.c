99a23ef52f9f7545f77589ef290ed4d2a0566bf6
Kamerelinda <kamerelinda14@gmail.com>
2023-04-17 21:02:45 +0000

    initializes a variable

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,23 @@
+#include "dog.h"
+
+/**
+ * init_dog - function that initializes a struct
+ *
+ * @struct dog -structure and tag
+ *
+ * @d: pointer
+ *
+ * @name: name of dog
+ *
+ * @age: age of dog
+ *
+ * @owner: owner of dog
+ *
+ * Return: void
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
