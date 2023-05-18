ca9991a8dde754a33c6249e027b26eb58574ddbd
Kamerelinda <kamerelinda14@gmail.com>
2023-05-18 04:44:08 +0000

    requirement

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,7 +10,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
 	ssize_t rd;
-	char *buf = malloc(sizeof(char) * letters);
+	char *buf;
 
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
@@ -19,24 +19,28 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (filename == NULL)
 		return (0);
 
-	rd = read(fd, buf, letters);
+	buf = malloc(sizeof(char) * letters);
 	if (buf == NULL)
 	{
 		close(fd);
 		return (0);
 	}
+
+	rd = read(fd, buf, letters);
 	if (rd == -1)
 	{
 		free(buf);
 		close(fd);
 		return (0);
 	}
+
 	if (write(STDOUT_FILENO, buf, rd) != rd)
 	{
 		free(buf);
 		close(fd);
 		return (0);
 	}
+
 	free(buf);
 	close(fd);
 	return (rd);
